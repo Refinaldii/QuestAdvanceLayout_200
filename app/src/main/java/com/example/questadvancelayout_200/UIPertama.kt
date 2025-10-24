6294a04ba3c06c07e44a16f60f0c62a456794ecd
Refinaldii <refinaldi9786@gmail.com>
2025-10-24 13:25:32 +0700

    tampilkan gambar profil di dalam card

--- app/src/main/java/com/example/questadvancelayout_200/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_200/UIPertama.kt
@@ -44,6 +44,14 @@ fun ActivityPertama(modifier: Modifier = Modifier) {
         )
     )  {
         Row {
+            val gambar = painterResource(id = R.drawable.ratatuli)
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(5.dp)
+            )
 
         }
     }

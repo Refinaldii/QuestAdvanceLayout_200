d73b4b8f1f90d760763934a4db31c9f09db05ea2
Refinaldii <refinaldi9786@gmail.com>
2025-10-24 13:21:37 +0700

    Nambahin spacer antara text dan card

--- app/src/main/java/com/example/questadvancelayout_200/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_200/UIPertama.kt
@@ -34,5 +34,5 @@ fun ActivityPertama(modifier: Modifier = Modifier) {
         text = stringResource(id = R.string.univ),
         fontSize = 22.sp
     )
-
+    Spacer(modifier = Modifier.height(25.dp))
 }
